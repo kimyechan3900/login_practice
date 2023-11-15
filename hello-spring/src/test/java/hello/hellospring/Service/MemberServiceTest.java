4c34f37d0a49316da6802448138b449e2b9e5345
kimyechan <tioon74@naver.com>
2023-11-16 02:02:03 +0900

    FEAT : 자바 버전 업데이트

--- hello-spring/src/test/java/hello/hellospring/Service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/Service/MemberServiceTest.java
@@ -2,13 +2,8 @@ package hello.hellospring.Service;
 
 import hello.hellospring.Model.Member;
 import hello.hellospring.service.MemberService;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MemberServiceTest {
