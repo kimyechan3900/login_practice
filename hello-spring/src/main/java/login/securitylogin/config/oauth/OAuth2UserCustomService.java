8c01051a537f8e649e314449d3bdf0f03da3b5d1
kimyechan <tioon74@naver.com>
2024-01-23 21:21:07 +0900

    Refactor : OAuth2 Kakao 코드 수정

--- hello-spring/src/main/java/login/securitylogin/config/oauth/OAuth2UserCustomService.java
+++ hello-spring/src/main/java/login/securitylogin/config/oauth/OAuth2UserCustomService.java
@@ -28,17 +28,17 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User user = super.loadUser(userRequest); // ❶ 요청을 바탕으로 유저 정보를 담은 객체 반환
 
-        System.out.println(userRequest.getClientRegistration());
+        System.out.println(user);
+        System.out.println("Access Token: " + userRequest.getAccessToken().getTokenValue());
         String provider = userRequest.getClientRegistration().getRegistrationId(); // 제공자 확인(kakao, google)
 
-        //saveOrUpdate(user,provider); // DB 업데이트 작업
+        saveOrUpdate(user,provider); // DB 업데이트 작업
 
         return user;
     }
 
     // ❷ 유저가 있으면 업데이트, 없으면 유저 생성
     private User saveOrUpdate(OAuth2User oAuth2User, String provider) {
-        Map<String, Object> attributes = oAuth2User.getAttributes(); // OAuth2User에서 속성 정보 가지고 옴
 
         String email;
         String name;
@@ -48,10 +48,10 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
             name = (String) oAuth2User.getAttributes().get("name"); // 구글이 제공하는 이름 정보
 
         } else if (provider.equals("kakao")) {
-            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-            email = (String) oAuth2User.getAttributes().get("id"); // 카카오가 제공하는 이메일 정보
-            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
-            name = (String)  oAuth2User.getAttributes().get("id"); // 카카오가 제공하는 이름 정보
+            Map<String, Object> kakaoAccount = (Map<String, Object>) oAuth2User.getAttributes().get("kakao_account");
+            email = (String) kakaoAccount.get("email"); // 카카오가 제공하는 이메일 정보
+            name = (String) ((Map<String, Object>) kakaoAccount.get("profile")).get("nickname"); // 카카오가 제공하는 이름 정보
+
         } else {
             throw new OAuth2AuthenticationException(new OAuth2Error("invalid_provider", "Invalid provider: " + provider, null));
         }
